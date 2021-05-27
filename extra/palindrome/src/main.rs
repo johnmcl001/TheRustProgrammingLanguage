603c6a8fd2f31687a96a625ea9b9dcaf18cde84c
John McLoughlin <john.mcloughlin.03@gmail.com>
2021-05-27 18:21:12 +0100

    extra: palindrome checker iterative and recursive done

--- extra/palindrome/src/main.rs
+++ extra/palindrome/src/main.rs
@@ -1,31 +1,36 @@
 fn main() {
     let not_a_palindrome = "Hello, world!";
-    let palindrome = "madam, i'm adam";
+    let palindrome = "Madam, I'm Adam";
+    println!("Recursive check");
     println!("{} {} palindrome", not_a_palindrome, if is_palindrome_rec(not_a_palindrome) {"is a"} else {"is not a"});
     println!("{} {} palindrome", palindrome, if is_palindrome_rec(palindrome) {"is a"} else {"is not a"});
+    println!("\nIterative check");
+    println!("{} {} palindrome", not_a_palindrome, if is_palindrome_iter(not_a_palindrome) {"is a"} else {"is not a"});
+    println!("{} {} palindrome", palindrome, if is_palindrome_iter(palindrome) {"is a"} else {"is not a"});
+}
+
+fn is_palindrome_iter(str: &str) -> bool {
+    let str_cleaned = is_palindrome_helper(str);
+    str_cleaned.chars()
+        .zip(str_cleaned.chars().rev())
+        .all(|x| x.0 == x.1)
 }
 
 fn is_palindrome_rec(str: &str) -> bool {
-    let str_cleaned: String = str.chars()
-        .map(|x| match x {
-            'A'..='Z' => x,
-            'a'..='z' => x,
-            _ => '\0'
-        }).collect();
-    println!("{}", str_cleaned);
-    is_palindrome_rec_helper(&str_cleaned)
+    check_palindrome_rec(is_palindrome_helper(str))
 }
 
-fn is_palindrome_rec_helper(str: &str) -> bool {
-    println!("{}", str);
-    if str.len() <= 1 {
-        println!("HERE");
-        return true;
-    }
-
+fn check_palindrome_rec(str: String) -> bool {
     if str.chars().next().unwrap() != str.chars().last().unwrap() {
         return false;
     }
 
-    is_palindrome_rec_helper(&str[1..str.len() - 1])
+    str.len() <= 1 || is_palindrome_rec(&str[1..str.len() - 1])
+}
+
+fn is_palindrome_helper(str: &str) -> String {
+    str.chars()
+        .filter(|x| x.is_ascii_alphanumeric())
+        .map(|x| x.to_lowercase().to_string())
+        .collect()
 }
