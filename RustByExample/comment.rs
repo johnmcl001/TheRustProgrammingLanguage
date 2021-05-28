879feb4ce0ef0b64007117d55dfd69c098e12bf7
John McLoughlin <john.mcloughlin.03@gmail.com>
2021-05-28 16:48:11 +0100

    Beginnning Rust By Example

---
+++ RustByExample/comment.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // This is an example of aline comment
+
+    /*
+    This is a block comment
+    */
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
