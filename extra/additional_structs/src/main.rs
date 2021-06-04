435a59baad72575dd9d943c6f1caec64e6f0513e
John McLoughlin <john.mcloughlin.03@gmail.com>
2021-06-04 22:33:45 +0100

    updated Triangle with impl and methods

--- extra/additional_structs/src/main.rs
+++ extra/additional_structs/src/main.rs
@@ -11,27 +11,28 @@ struct Triangle {
     v3: Point,
 }
 
+impl Triangle {
+    fn is_triangle_valid(&self) -> bool {
+        let e_v1_v2 = self.edge(&self.v1, &self.v2);
+        let e_v1_v3 = self.edge(&self.v1, &self.v3);
+        let e_v2_v3 = self.edge(&self.v2, &self.v3);
+
+        e_v1_v2 + e_v1_v3 > e_v2_v3 &&
+        e_v1_v3 + e_v2_v3 > e_v1_v2  &&
+        e_v2_v3 + e_v1_v2 > e_v1_v3 
+    }
+
+    fn edge(&self, p1: &Point, p2: &Point) -> f64 {
+        let (x1, y1, x2, y2) = (f64::from(p1.x), f64::from(p1.y), f64::from(p2.x), f64::from(p2.y));
+        ((x2 - x1).powf(2.0) + (y2 - y1).powf(2.0)).sqrt()
+    }
+}
+
 fn given_point(x: i32, y: i32) -> Point {
     Point {
         x,
         y
     }
-}
-
-fn is_triangle_valid(triangle: Triangle) -> bool {
-    let (v1, v2, v3) = (triangle.v1, triangle.v2, triangle.v3);
-    let e_v1_v2 = distance(&v1, &v2);
-    let e_v1_v3 = distance(&v1, &v3);
-    let e_v2_v3 = distance(&v2, &v3);
-
-    e_v1_v2 + e_v1_v3 > e_v2_v3 &&
-    e_v1_v3 + e_v2_v3 > e_v1_v2  &&
-    e_v2_v3 + e_v1_v2 > e_v1_v3 
-}
-
-fn distance(p1: &Point, p2: &Point) -> f64 {
-    let (x1, y1, x2, y2) = (f64::from(p1.x), f64::from(p1.y), f64::from(p2.x), f64::from(p2.y));
-    ((x2 - x1).powf(2.0) + (y2 - y1).powf(2.0)).sqrt()
 }
 
 fn main() {
@@ -47,6 +48,6 @@ fn main() {
         v3: given_point(1, 5),
     };
 
-    println!("valid triangle should be true: {}", is_triangle_valid(valid_triangle));
-    println!("invalid triangle should be false: {}", is_triangle_valid(invalid_triangle));
+    println!("valid triangle should be true: {}", valid_triangle.is_triangle_valid());
+    println!("invalid triangle should be false: {}", invalid_triangle.is_triangle_valid());
 }
