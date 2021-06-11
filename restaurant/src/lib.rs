7fa39968e50c6e7a95b132cba2ef082fc64fbab3
John McLoughlin <john.mcloughlin.03@gmail.com>
2021-06-11 11:47:38 +0100

    visibility exercises, linked list and constants

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -9,7 +9,25 @@ mod front_of_house {
         fn take_order() {}
         fn serve_order() {}
         fn take_payment() {}
+
+        mod back_of_house {
+            fn fix_incorrect_order() {
+                cook_order();
+                super::serve_order();
+            }
+
+            fn cook_order(){}
+        }
+
     }
+}
+
+pub fn eat_at_restaurant() {
+    // absolute
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative
+    front_of_house::hosting::add_to_waitlist();
 }
 
 
