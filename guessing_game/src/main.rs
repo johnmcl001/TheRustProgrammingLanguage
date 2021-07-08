fd0a0f01d68dd6a1084255af72a3168cdbbd71e1
John McLoughlin <john.mcloughlin.03@gmail.com>
2021-07-08 07:57:04 +0100

    Added Guess struct to guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,10 +2,30 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 and 100, got {}", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number.");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number);
 
     loop {
@@ -16,14 +36,14 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read input");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
